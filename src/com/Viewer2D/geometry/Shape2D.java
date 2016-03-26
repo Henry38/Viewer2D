fd830e2891dc74e302d09e73ea9b9929cdc8c4a7
Henry38 <lefevrehenry@hotmail.fr>
2016-03-26 14:51:09 +0100

    Update Viewer2D.jar

--- src/com/Viewer2D/geometry/Shape2D.java
+++ src/com/Viewer2D/geometry/Shape2D.java
@@ -1,6 +1,7 @@
 package com.Viewer2D.geometry;
 
 import java.awt.Color;
+import java.awt.geom.Rectangle2D;
 
 import math2D.Point2D;
 import math2D.Transformation2D;
@@ -52,6 +53,41 @@ public class Shape2D {
 	/** Retourne la couleur de la Shape2D */
 	public Color getColor() {
 		return color;
+	}
+	
+	/** Calcul et retourne la boundingBox */
+	public Rectangle2D.Double computeBoundingBox() {
+		double minX = Double.MAX_VALUE;
+		double minY = Double.MAX_VALUE;
+		double maxX = Double.MIN_VALUE;
+		double maxY = Double.MIN_VALUE;
+		for (int i = 0; i < npoints; i++) {
+			Point2D point = getPoint(i);
+			minX = Math.min(minX, point.getX());
+			minY = Math.min(minY, point.getY());
+			maxX = Math.max(maxX, point.getX());
+			maxY = Math.max(maxY, point.getY());
+		}
+		return new Rectangle2D.Double(minX, minY, maxX-minX, maxY-minY);
+	}
+	
+	/** Retourne vrai si le point (x, y) est a l'interieur du polygone */
+	public boolean isInside(double x, double y) {
+		if (!computeBoundingBox().contains(x, y)) {
+			return false;
+		}
+		boolean inside = false;
+		int j = getNPoint() - 1;
+		for (int i = 0; i < getNPoint(); i++) {
+			Point2D iPoint = getPoint(i);
+			Point2D jPoint = getPoint(j);
+			if ( ((iPoint.getY() > y) != (jPoint.getY() > y)) &&
+					(x < (jPoint.getX()-iPoint.getX()) * (y-iPoint.getY()) / (jPoint.getY()-iPoint.getY()) + iPoint.getX()) ) {
+				inside = !inside;
+			}
+			j = i;
+		}
+		return inside;
 	}
 	
 	/** Met a jour la coordonnee X de l'oigine */
